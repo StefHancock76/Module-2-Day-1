9e99e714f6150294eb1b6a772d82a9118c9a1540
StefHancock76 <stefshubert76@gmail.com>
2023-07-15 21:12:11 -0500

    Updating for class

---
+++ calculator.js
@@ -0,0 +1,39 @@
+let user_value1 = Number(prompt("enter value 1"));
+  
+let user_value2 = Number(prompt("enter value 2"));
+  
+
+  
+function add(number1, number2){
+  
+  return number1 + number2;
+  
+}
+  
+
+  
+function subs(num1, num2) {
+  
+  return num1-num2;
+  
+}
+    
+  
+function multiply(n1,n2) {
+  
+  return n1*n2;
+  
+}
+  
+ 
+  
+function div(num1, num2){
+  
+  return num1/num2;
+  
+}
+  
+
+  
+console.log(`The addition of your input is ${add(user_value1, user_value2)}`)
+  
---
+++ jsday1.js
@@ -0,0 +1,31 @@
+console.log('Hello Everybody!');
+const num = 1;
+console.log("num",typeof num);
+
+const str = "1";
+console.log("str",typeof str);
+
+const bool = true;
+console.log("bool",typeof bool);
+
+let x;
+console.log("x",typeof x);
+
+const y = null;
+console.log("y",typeof y);
+
+const arr = ["Apple", "Banana", "Orange"];
+console.log("arr",typeof arr);
+
+const obj = {firstName: "John", lastName: "Smith"};
+console.log("obj",typeof obj);
+
+
+let firstNum = 17;
+let secondNum = 4;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
