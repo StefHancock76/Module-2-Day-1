a271db4ab020100e9d723f841066a35dfdf03584
StefHancock76 <stefshubert76@gmail.com>
2023-07-24 08:30:36 -0500

    Working on assignment

---
+++ average.js
@@ -0,0 +1,8 @@
+var average = ("53" + "96" + "65" + "74" / "4");
+console.log(average);
+
+
+function add(grade1, grade2, grade3, grade4){  
+    return grade1 + grade2 + grade3 + grade4;    
+  }
+  console.log(add(grade1 + grade2 + grade3 + grade4))
---
+++ index_Stefanie.js
@@ -0,0 +1,19 @@
+let studentFirstName = "Brandon";
+let studentLastName = "N";
+let grade1 = 53;
+let grade2 = 96;
+let grade3 = 65;
+let grade4 = 74;
+let passingGrade = 80;
+//1.Create a variable called average that computes for the average from grade1 to grade4
+var average = ("53" + "96" + "65" + "74");
+console.log(average / "4");
+//2. Change the value of studentLastName to "Newman" 
+
+studentLastName = "Newman";
+console.log("Last Name:",studentLastName);
+
+//3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
+//ex. Jane Doe has an average grade of 100.
+console.log(`${studentFirstName + studentLastName}has an average of${[average]}`)
+//4. Write code that would answer this question: Did the student pass?
